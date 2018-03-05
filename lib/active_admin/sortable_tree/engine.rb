fed8a00e061f4239b3b724ec802802ec6579c42e
Brian J. Dillard <armchairdj@gmail.com>
2018-03-05 13:36:29 -0800

    - Remove deprecated active_admin code.

--- lib/active_admin/sortable_tree/engine.rb
+++ lib/active_admin/sortable_tree/engine.rb
@@ -4,18 +4,6 @@ module ActiveAdmin
   module SortableTree
     class Engine < ::Rails::Engine
       engine_name "active_admin-sortable_tree"
-
-      initializer "active_admin-sortable_tree.precompile", group: :all do |app|
-        app.config.assets.precompile += [
-          "active_admin/sortable.css",
-          "active_admin/sortable.js"
-        ]
-      end
-
-      initializer "active_admin-sortable_tree.register_assets" do
-        ActiveAdmin.application.register_stylesheet "active_admin/sortable.css"
-        ActiveAdmin.application.register_javascript "active_admin/sortable.js"
-      end
     end
   end
 end
